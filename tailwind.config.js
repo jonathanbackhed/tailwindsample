13e4dff9443697c22c787af30640d12c7e5e12ef
Jonathan Bäckhed <1368618+jonathanbackhed@users.noreply.github.com>
2021-03-08 02:13:04 +0100

    automatic dark/light mode added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
 module.exports = {
   purge: {
     enabled: true,
-    content: ['./*.html']
+    content: ["./*.html"]
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "media", // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         brand: {
           light: "#3fbaeb", // font-brand-light
           DEFAULT: "#0fa9e6", // font-brand
-          dark: "#0c87b8"
+          dark: "#0c87b8",
+          darker: "#0a6b91",
+        },
+        dark: {
+          DEFAULT: "#181818",
+          light: "#202020",
+          lighter: "#313131",
         },
       },
     },
